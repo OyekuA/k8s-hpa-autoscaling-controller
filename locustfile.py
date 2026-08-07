38138cd63f10e2da970bab60aa3005610f873aff
oyekua <abdulquadrioyeku1@gmail.com>
2026-08-07 18:25:01 +0100

    feat: add Locust load testing script for /compute-heavy endpoint

--- locustfile.py
+++ locustfile.py
@@ -1,1 +1,9 @@
-# placeholder - Load test script
+from locust import HttpUser, constant, task
+
+
+class ComputeHeavyUser(HttpUser):
+    wait_time = constant(0)
+
+    @task
+    def compute_heavy(self):
+        self.client.get("/compute-heavy")
